2e4b59018a8b9f6dcb400d0405edde8097fda599
fazt <me@faztweb.com>
2017-12-22 17:29:46 -0500

    static files added

--- src/config/server.js
+++ src/config/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../app/views'));
-
 // middlewares
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '../static')))
 
 module.exports = app;
